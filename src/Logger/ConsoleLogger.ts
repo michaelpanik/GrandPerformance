8471ddf3c65309d026a22b13a45fd41418aff98c
Michael Panik <michael@michael-panik.com>
2021-12-27 07:53:53 -0600

    Show full depth in console logger

--- src/Logger/ConsoleLogger.ts
+++ src/Logger/ConsoleLogger.ts
@@ -3,7 +3,7 @@ import { LogRecord } from "./Logger.d";
 
 class ConsoleLogger extends Logger {
   log(records: LogRecord[]): void {
-    console.log(records);
+    console.log(JSON.stringify(records, null, 2));
   }
 }
 
